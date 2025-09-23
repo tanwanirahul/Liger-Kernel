551c7c3c6638fe05e2140548ee6c8b0e4361b65f
Rahul Tanwani <tanwanirahul@gmail.com>
2025-09-23 12:58:15 +0530

    Clip rewards for negative solutions and boost up positive rewards for chosen solutions

--- src/liger_kernel/chunked_loss/dpo_loss.py
+++ src/liger_kernel/chunked_loss/dpo_loss.py
@@ -48,7 +48,7 @@ class LigerFusedLinearDPOFunction(LigerFusedLinearPreferenceBase):
         chosen_logratios = (chosen_logps - ref_chosen_logps) * logprob_scaling_factor
         rejected_logratios = (rejected_logps - ref_rejected_logps) * logprob_scaling_factor
 
-        max_reward_for_rejected_logratio = -1 / beta
+        max_reward_for_rejected_logratio = -0.5 / beta
         clipped_rejected_logratios = rejected_logratios
 
 
@@ -66,7 +66,7 @@ class LigerFusedLinearDPOFunction(LigerFusedLinearPreferenceBase):
             if chosen_rewards < 0:
                 logits_diff = chosen_rewards - minimum_loss_for_negative_chosen_rewards
             else:
-                logits_diff = beta * (chosen_logratios - clipped_rejected_logratios)
+                logits_diff = beta * ((2 * chosen_logratios) - clipped_rejected_logratios)
             loss = -F.logsigmoid(logits_diff).sum() / (full_target.shape[0] // 2)
 
         elif loss_type == "apo_zero":
